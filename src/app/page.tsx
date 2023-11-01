76cf81eddee3ba101f6796971a4043a47531e36c
Alex <skyriver.f1@gmail.com>
2023-11-02 00:02:05 +0500

    added about-company & more fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Hero from "@/components/Hero/Hero";
 import ProductList from "@/components/ProductList/ProductList";
 import ServicesList from "@/components/ServicesList/ServicesList";
+import AboutCompany from "@/components/AboutCompany/AboutCompany";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero/>
       <ProductList/>
       <ServicesList/>
+      <AboutCompany/>
     </main>
   )
 }
---
+++ src/components/AboutCompany/AboutCompany.tsx
@@ -0,0 +1,35 @@
+"use-client";
+
+import React from 'react';
+import TitleBlocks from '../TitleBlocks/TitleBlocks';
+import Image from 'next/image';
+import './AboutCompany.scss'
+
+function AboutCompany() {
+  return (
+    <div className="about-company">
+      <div className="container">
+        <TitleBlocks title="О КОМПАНИИ"/>
+        <div className="about-company__inner">
+          <div className="about-company__img">
+            <Image
+              src="./about-company-pic.svg"
+              alt="О КОМПАНИИ"
+              width="574"
+              height="574"
+              priority
+            />
+          </div>
+          <div className="about-company__content">
+            <p>
+              Группа компаний MEDOL создавалась высококвалифицированными специалистами в сфере медицины, инженерии и экономики, за плечами которых значительный опыт на рынке высоких медицинских технологий, которая имеет свои представительства в разных уголках Земли. В 2011 году MEDOL зарегистрировал в Узбекистане ИП ООО “Medical Online Services". Цель компании построить прозрачный долгосрочный бизнес, принести пользу обществу путем развития и внедрения передовых технологий в систему здравоохранения Республики Узбекистан. 
+            </p>
+            <a className="button-default" href="#">Узнать больше</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutCompany
